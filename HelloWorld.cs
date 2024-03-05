ab9fbd823fa9f42143a0bf38361140e9346749bd
James Vernon <Vernon.James@SPCollege.edu>
2024-03-05 14:49:28 -0500

    added hellowworld class

---
+++ HelloWorld.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumbers
+{
+    public class HelloWorld
+    {
+        private string _message;
+        
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+            set
+            {
+                _message = value;
+            }
+        }
+        
+        public HelloWorld(string message) { 
+        
+            _message=message;
+        }
+    }
+}
